da6fa4fe7bcf0c4071e0d21be17f493fa90e8fd5
ThameemHussain <90094742+ThameemHussain@users.noreply.github.com>
2021-10-02 12:42:26 +0530

    vandhurucha? da?4th commit

--- app/src/main/java/com/example/thenewcollege/authentication_activity.java
+++ app/src/main/java/com/example/thenewcollege/authentication_activity.java
@@ -21,8 +21,6 @@ public class authentication_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_authentication);
         login=(Button)findViewById(R.id.btnlogin);
--- app/src/main/java/com/example/thenewcollege/masjidadminpanel_activity.java
+++ app/src/main/java/com/example/thenewcollege/masjidadminpanel_activity.java
@@ -12,8 +12,6 @@ public class masjidadminpanel_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_masjidadminpanel);
         EditText fajr=findViewById(R.id.etfajr);
